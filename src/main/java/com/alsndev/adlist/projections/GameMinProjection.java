5d3502ea66e226b0d240ddbc242ca12fc166f85d
Alisson Alves <alisson.asilva3@gmail.com>
2025-05-21 21:48:16 -0300

    holog success

--- src/main/java/com/alsndev/adlist/projections/GameMinProjection.java
+++ src/main/java/com/alsndev/adlist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.alsndev.adlist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
