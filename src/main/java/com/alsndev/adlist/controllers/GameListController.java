b821984bdb4f61523eaca5beae8094c146e594fe
Alisson Alves <alisson.asilva3@gmail.com>
2025-05-25 20:53:20 -0300

    move and reorder games from list

--- src/main/java/com/alsndev/adlist/controllers/GameListController.java
+++ src/main/java/com/alsndev/adlist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package com.alsndev.adlist.controllers;
 
 import com.alsndev.adlist.dto.GameListDTO;
 import com.alsndev.adlist.dto.GameMinDTO;
+import com.alsndev.adlist.dto.ReplacementDTO;
 import com.alsndev.adlist.services.GameListService;
 import com.alsndev.adlist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +30,11 @@ public class GameListController {
     public List<GameMinDTO> searchByList(@PathVariable Long listId) {
         List<GameMinDTO> result = gameService.searchByList(listId);
         return result;
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 
 }
--- src/main/java/com/alsndev/adlist/services/GameListService.java
+++ src/main/java/com/alsndev/adlist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.alsndev.adlist.services;
 
 import com.alsndev.adlist.dto.GameListDTO;
 import com.alsndev.adlist.entities.GameList;
+import com.alsndev.adlist.projections.GameMinProjection;
 import com.alsndev.adlist.repositories.GameListRepository;
+import com.alsndev.adlist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +13,8 @@ import java.util.List;
 
 @Service
 public class GameListService {
+    @Autowired
+    private GameRepository gameRepository;
 
     @Autowired
     private GameListRepository gameListRepository;      //Instancia de GameListRepository, injetando a instancia dentro do GameListService.
@@ -19,5 +23,19 @@ public class GameListService {
     public List<GameListDTO> searchAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(GameListDTO::new).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection gameObj = list.remove(sourceIndex);
+        list.add(destinationIndex, gameObj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for(int i = min; i <= max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
